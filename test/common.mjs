296e16e603411cc83388175d8f4345cb47c61ccc
David Herron <david@davidherron.com>
2025-01-15 15:35:47 +0200

    Add fileNoContains

--- test/common.mjs
+++ test/common.mjs
@@ -6,9 +6,21 @@ export async function fileContains(fn, containsList) {
 
     for (const contains of containsList) {
         if (txt.indexOf(contains) >= 0) {
-            return true;
+            // return true;
         } else {
             throw new Error(`file ${fn} did not contain ${contains}`);
         }
     }
+    return true;
+}
+
+export async function fileNoContains(fn, containsList) {
+    const txt = await fsp.readFile(fn, 'utf8');
+
+    for (const contains of containsList) {
+        if (txt.indexOf(contains) >= 0) {
+            throw new Error(`file ${fn} contained unwanted content ${contains}`);
+        }
+    }
+    return true;
 }
